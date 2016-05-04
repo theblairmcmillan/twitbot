52b42ec092d765e3333ff38c524dbd812075f5e3
BlairMcMillan <theblairmcmillan@gmail.com>
2016-05-03 23:22:19 -0500

    day timer tweeting followers

--- bot.js
+++ bot.js
@@ -1,45 +1,43 @@
-console.log("bot is working");
+console.log('The follow bot is starting');
 
 var Twit = require('twit');
-var config = require('./config');
 
+var config = require('./config');
 var T = new Twit(config);
 
-var params = {
-	q: 'oprah',
-	count: 1
+// Setting up a user stream
+var stream = T.stream('user');
+
+// Anytime someone follows me
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+  console.log("Follow event!");
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  tweetIt('.@' + screenName + ' I love bread');
 }
 
 
-// searching for 1 tweet containing twitter //
-T.get('search/tweets', params, gotData);
-
-function gotData(err, data, response) {
-	var tweets = data.statuses;
-	for (var i = 0; i < tweets.length; i++) {
-		console.log(tweets[i].text);
-	}
-}
-
-setInterval(tweetIt, 1000*60*60*24)
-
-
-var r = Math. floor(Math.random()*100);
 tweetIt();
+setInterval(tweetIt, 1000*60*60*24);
+console.log("getting to tweet it repeat");
 
 function tweetIt() {
-// creating tweet and posting //
+
+	var r = Math.floor(Math.random()*100);
+
 	var tweet = {
-		status:  r + '#OprahGiffrey'
+	  status: "bread bread bread" + r
 	}
 
 	T.post('statuses/update', tweet, tweeted);
 
 	function tweeted(err, data, response) {
-		if(err) {
-			console.log("something went wring");
-		} else {
-			console.log("it worked");
-		}
+	  if (err) {
+	  	console.log("Something went wrong!");
+	  } else {
+	    console.log("It worked!");
+	  }
 	}
-};
+}
