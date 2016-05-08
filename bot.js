98d303f2940d7c506bd0ca3fe68bbebc477d2cb7
BlairMcMillan <theblairmcmillan@gmail.com>
2016-05-08 13:17:00 -0500

    all twitter functions working

--- bot.js
+++ bot.js
@@ -1,30 +1,21 @@
 console.log('The bot is starting');
 
 var Twit = require('twit');
-
 var config = require('./config');
 var T = new Twit(config);
-
 // Setting up a user stream
 var stream = T.stream('user');
-
-// Anytime someone follows me
-stream.on('follow', followed);
 
 function followed(eventMsg) {
   console.log("Follow event!");
   var name = eventMsg.source.name;
   var screenName = eventMsg.source.screen_name;
   tweetIt('.@' + screenName + ' I love bread');
-}
-
-
-// handling replies to twitter bot //
-stream.on('tweet', tweetEvent);
+};
 
 function tweetEvent(eventMsg) {
-	// var fs = require('fs');
-	// var json = JSON.stringify(eventMsg,null,2);
+	var fs = require('fs');
+	var json = JSON.stringify(eventMsg,null,2);
 	// fs.writeFile("tweet.json", json);
 	var replyto = eventMsg.in_reply_to_screen_name;
 	var text = eventMsg.text;
@@ -32,24 +23,23 @@ function tweetEvent(eventMsg) {
 	console.log(replyto + ' ' + from);
 
 	if(replyto === "OprahGiffrey") {
-		var newtweet = '@' + from + 'send bread!';
+		var newtweet = '@' + from + ' send bread!';
 		tweetIt(newtweet);
 	}
-}
+};
 
-// calling tweetIt and setting interval to one day// 
-tweetIt();
-setInterval(tweetIt, 1000*60*60*24);
-console.log("getting to tweet it repeat");
-
-function tweetIt(text) {
-
+function dailyTweet() {
 	// generating random number to identify each tweet//
 	var r = Math.floor(Math.random()*100);
+	var status = "bread bread bread" + r;
+	tweetIt(status);
+}
+
+function tweetIt(txt) {
 
 	var tweet = {
-	  status: "bread bread bread" + r
-	}
+	  status: txt
+	};
 
 	T.post('statuses/update', tweet, tweeted);
 
@@ -60,4 +50,15 @@ function tweetIt(text) {
 	    console.log("It worked!");
 	  }
 	}
-}
+};
+
+// Anytime someone follows me
+stream.on('follow', followed);
+// handling replies to twitter bot //
+stream.on('tweet', tweetEvent);
+
+// calling tweetIt and setting interval to one day// 
+dailyTweet();
+setInterval(dailyTweet, 1000*60*60*24);
+console.log("getting to tweet it set interval function");
+
